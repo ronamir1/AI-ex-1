6a3926d7c880dc4114a3561cdbc8d4b5785908a6
Dan Eliahu <daneliyahu@cs.huji.ac.il>
2021-04-07 12:48:50 +0300

    Search using nodes

--- blokus/search.py
+++ blokus/search.py
@@ -3,7 +3,6 @@ In search.py, you will implement generic search algorithms
 """
 
 import util
-import numpy as np
 
 
 class SearchProblem:
@@ -50,6 +49,17 @@ class SearchProblem:
         util.raiseNotDefined()
 
 
+class SearchNode:
+    """
+    Search node containing the relevant information for that node
+    """
+
+    def __init__(self, state, actions, cost=0):
+        self.state = state
+        self.actions = actions
+        self.cost = cost
+
+
 def depth_first_search(problem):
     """
     Search the deepest nodes in the search tree first.
@@ -76,32 +86,25 @@ def breadth_first_search(problem):
     return search(problem, fringe)
 
 
-
 def uniform_cost_search(problem):
     """
     Search the node of least total cost first.
     """
-    actions = []
-    prev = dict()  # son state to father state
-    costs = dict()
     fringe = util.PriorityQueue()
-    fringe.push(problem.get_start_state(), 0)
-    costs[problem.get_start_state()] = 0
+    fringe.push(SearchNode(problem.get_start_state(), []), 0)
+    visited = set()
     while not fringe.isEmpty():
-        cur_state = fringe.pop()
-        if problem.is_goal_state(cur_state):
-            while not cur_state == problem.get_start_state():  # build actions list from goal state
-                cur_state, cur_move = prev[cur_state]
-                actions.append(cur_move)
-            actions.reverse()
-            return actions
-        for suc in problem.get_successors(cur_state):  # expand graph
-            if suc[0] not in prev:
-                prev[suc[0]] = (cur_state, suc[1])
-                cost = costs[cur_state] + suc[2]
-                costs[suc[0]] = cost
-                fringe.push(suc[0], cost)
+        cur_node = fringe.pop()
+        if problem.is_goal_state(cur_node.state):  # goal state reached
+            return cur_node.actions
+        if cur_node.state not in visited:
+            for suc in problem.get_successors(cur_node.state):
+                new_cost = cur_node.cost + suc[2]
+                # push successor to fringe with relevant list of actions
+                fringe.push(SearchNode(suc[0], cur_node.actions + [suc[1]], new_cost), new_cost)
+            visited.add(cur_node.state)
 
+    return []  # no goal found
 
 
 def null_heuristic(state, problem=None):
@@ -117,50 +120,41 @@ def a_star_search(problem, heuristic=null_heuristic):
     Search the node that has the lowest combined cost and heuristic first.
     """
     "*** YOUR CODE HERE ***"
-    actions = []
-    prev = dict()  # son state to father state
-    costs = dict()
     fringe = util.PriorityQueue()
-    fringe.push(problem.get_start_state(), 0)
-    costs[problem.get_start_state()] = 0
-    prev[problem.get_start_state()] = None
+    fringe.push(SearchNode(problem.get_start_state(), []), 0)
+    visited = set()
     while not fringe.isEmpty():
-        cur_state = fringe.pop()
-        if problem.is_goal_state(cur_state):
-            while not cur_state == problem.get_start_state():  # build actions list from goal state
-                cur_state, cur_move = prev[cur_state]
-                actions.append(cur_move)
-            actions.reverse()
-            return actions
-        for suc in problem.get_successors(cur_state):  # expand graph
-            if suc[0] not in prev:
-                cost = costs[cur_state] + suc[2]
-                costs[suc[0]] = cost
-                fringe.push(suc[0], cost + heuristic(suc[0], problem))
-                prev[suc[0]] = (cur_state, suc[1])
-    return actions
+        cur_node = fringe.pop()
+        if problem.is_goal_state(cur_node.state):  # goal state reached
+            return cur_node.actions
+        if cur_node.state not in visited:
+            for suc in problem.get_successors(cur_node.state):
+                new_cost = cur_node.cost + suc[2]
+                # push successor to fringe with relevant list of actions
+                fringe.push(SearchNode(suc[0], cur_node.actions + [suc[1]], new_cost), new_cost + heuristic(suc[0], problem))
+            visited.add(cur_node.state)
+
+    return []  # no goal found
 
 
 def search(problem, fringe):
-    actions = []
-    prev = dict()  # son state to father state
-    start_state = problem.get_start_state()
-    fringe.push(start_state)
-    prev[start_state] = None
+    """
+    General search algorithm which gets the search problem and fringe
+    Returns a list of actions to get from start state to a goal state
+    """
+    fringe.push(SearchNode(problem.get_start_state(), []))
+    visited = set()
     while not fringe.isEmpty():
-        cur_state = fringe.pop()
-        if problem.is_goal_state(cur_state):
-            while not cur_state == start_state:  # build actions list from goal state
-                cur_state, cur_move = prev[cur_state]
-                actions.append(cur_move)
-            actions.reverse()
-            return actions
-        for suc in problem.get_successors(cur_state):  # expand graph
-            if suc[0] not in prev:
-                prev[suc[0]] = (cur_state, suc[1])
-                fringe.push(suc[0])
+        cur_node = fringe.pop()
+        if problem.is_goal_state(cur_node.state):  # goal state reached
+            return cur_node.actions
+        if cur_node.state not in visited:
+            for suc in problem.get_successors(cur_node.state):
+                fringe.push(SearchNode(suc[0], cur_node.actions + [
+                    suc[1]]))  # push successor to fringe with relevant list of actions
+            visited.add(cur_node.state)
 
-
+    return []  # no goal found
 
 
 # Abbreviations
@@ -168,3 +162,84 @@ bfs = breadth_first_search
 dfs = depth_first_search
 astar = a_star_search
 ucs = uniform_cost_search
+
+# -------------------------- Prev Implementations ----------------
+
+# def search(problem, fringe):
+#     """
+#     General search algorithm which gets the search problem and fringe
+#     Returns a list of actions to get from start state to a goal state
+#     """
+#     actions = []
+#     prev = dict()  # son state to father state
+#     start_state = problem.get_start_state()
+#     fringe.push(start_state)
+#     prev[start_state] = None
+#     while not fringe.isEmpty():
+#         cur_state = fringe.pop()
+#         if problem.is_goal_state(cur_state):
+#             while not cur_state == start_state:  # build actions list from goal state
+#                 cur_state, cur_move = prev[cur_state]
+#                 actions.append(cur_move)
+#             actions.reverse()
+#             return actions
+#         for suc in problem.get_successors(cur_state):  # expand graph
+#             if suc[0] not in prev:
+#                 prev[suc[0]] = (cur_state, suc[1])
+#                 fringe.push(suc[0])
+
+
+# def a_star_search(problem, heuristic=null_heuristic):
+#     """
+#     Search the node that has the lowest combined cost and heuristic first.
+#     """
+#     "*** YOUR CODE HERE ***"
+#     actions = []
+#     prev = dict()  # son state to father state
+#     costs = dict()
+#     fringe = util.PriorityQueue()
+#     start_state = problem.get_start_state()
+#     fringe.push(start_state, 0)
+#     costs[start_state] = 0
+#     prev[start_state] = None
+#     while not fringe.isEmpty():
+#         cur_state = fringe.pop()
+#         if problem.is_goal_state(cur_state):
+#             while not cur_state == start_state:  # build actions list from goal state
+#                 cur_state, cur_move = prev[cur_state]
+#                 actions.append(cur_move)
+#             actions.reverse()
+#             return actions
+#         for suc in problem.get_successors(cur_state):  # expand graph
+#             if suc[0] not in prev:
+#                 cost = costs[cur_state] + suc[2]
+#                 costs[suc[0]] = cost
+#                 fringe.push(suc[0], cost + heuristic(suc[0], problem))
+#                 prev[suc[0]] = (cur_state, suc[1])
+#     return actions
+
+
+# def uniform_cost_search(problem):
+#     """
+#     Search the node of least total cost first.
+#     """
+#     actions = []
+#     prev = dict()  # son state to father state
+#     costs = dict()
+#     fringe = util.PriorityQueue()
+#     fringe.push(problem.get_start_state(), 0)
+#     costs[problem.get_start_state()] = 0
+#     while not fringe.isEmpty():
+#         cur_state = fringe.pop()
+#         if problem.is_goal_state(cur_state):
+#             while not cur_state == problem.get_start_state():  # build actions list from goal state
+#                 cur_state, cur_move = prev[cur_state]
+#                 actions.append(cur_move)
+#             actions.reverse()
+#             return actions
+#         for suc in problem.get_successors(cur_state):  # expand graph
+#             if suc[0] not in prev:
+#                 prev[suc[0]] = (cur_state, suc[1])
+#                 cost = costs[cur_state] + suc[2]
+#                 costs[suc[0]] = cost
+#                 fringe.push(suc[0], cost)
